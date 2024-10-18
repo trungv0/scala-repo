090c2163f26fb678d2bc5d98ce66c6b9ec668107
Trung V <demonkid98@gmail.com>
2024-10-18 15:50:10 +0200

    test spark import

--- src/test/scala/samples/junit.scala
+++ src/test/scala/samples/junit.scala
@@ -3,14 +3,26 @@ package samples
 import org.junit._
 import Assert._
 
+import org.apache.spark.sql.SparkSession
+
 @Test
 class AppTest {
 
     @Test
     def testOK() = assertTrue(true)
 
-//    @Test
-//    def testKO() = assertTrue(false)
+    @Test
+    def checkSparkSetup(): Unit = {
+
+        // Create spark session
+        val spark = SparkSession.builder().master("local").getOrCreate()
+        import spark.implicits._
+
+        val data = Seq(("1"), ("2")).toDF("c1")
+
+        data.show()
+        data.write.format("parquet").mode("overwrite").save("/tmp/data1")
+    }
 
 }
 
